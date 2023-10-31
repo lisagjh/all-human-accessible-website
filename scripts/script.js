67ce50b857cdad910f44aaa170e85d3f43e318d3
lisagjh <lisagjhoorn@outlook.com>
2023-10-31 14:06:11 +0100

    vertical carousels

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,19 @@ window.onclick = function (event) {
     }
   }
 };
+
+
+// carousel 
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
