39b81577ea3e66da2adacf7455435984bcd032f6
lisagjh <lisagjhoorn@outlook.com>
2023-11-02 09:44:38 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -20,15 +20,43 @@ window.onclick = function (event) {
 
 // carousel 
 
-const prev = document.getElementById('prev-btn')
-const next = document.getElementById('next-btn')
-const list = document.getElementById('item-list')
+const prev = document.getElementById('prev-btn1')
+const next = document.getElementById('next-btn1')
+const list = document.getElementById('item-list1')
+
+const prev2 = document.getElementById('prev-btn2')
+const next2 = document.getElementById('next-btn2')
+const list2 = document.getElementById('item-list2')
+
+const prev3 = document.getElementById('prev-btn3')
+const next3 = document.getElementById('next-btn3')
+const list3 = document.getElementById('item-list3')
+
 const itemWidth = 150
 const padding = 10
+
 
 prev.addEventListener('click',()=>{
   list.scrollLeft -= (itemWidth + padding)
 })
 next.addEventListener('click',()=>{
   list.scrollLeft += (itemWidth + padding)
+})
+
+
+prev2.addEventListener('click',()=>{
+  list2.scrollLeft -= (itemWidth + padding)
+})
+
+next2.addEventListener('click',()=>{
+  list2.scrollLeft += (itemWidth + padding)
+})
+
+
+prev3.addEventListener('click',()=>{
+  list3.scrollLeft -= (itemWidth + padding)
+})
+
+next3.addEventListener('click',()=>{
+  list3.scrollLeft += (itemWidth + padding)
 })
